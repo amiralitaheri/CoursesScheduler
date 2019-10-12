3a37ebca6d70e4542462775635ef51e304a6c37a
Amirali Taheri <amiralitaheri64@gmail.com>
2019-10-12 13:09:30 +0330

    initial commit

---
+++ app/src/main/java/ir/saatgroup/coursesscheduler/MainActivity.kt
@@ -0,0 +1,12 @@
+package ir.saatgroup.coursesscheduler
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
