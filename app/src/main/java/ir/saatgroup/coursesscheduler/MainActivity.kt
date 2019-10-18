79b792d4829211351c3b9375c84c3fca3e89178d
Amirali Taheri <amiralitaheri64@gmail.com>
2019-10-18 20:39:57 +0330

    added firebase auth ui

--- app/src/main/java/ir/saatgroup/coursesscheduler/MainActivity.kt
+++ app/src/main/java/ir/saatgroup/coursesscheduler/MainActivity.kt
@@ -2,11 +2,60 @@ package ir.saatgroup.coursesscheduler
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
+import android.app.Activity
+import android.content.Intent
+import android.view.View
+import com.firebase.ui.auth.IdpResponse
+import com.google.android.material.snackbar.Snackbar
+import ir.saatgroup.coursesscheduler.auth.signOut
+import ir.saatgroup.coursesscheduler.auth.providers
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val RC_SIGN_IN = 77
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val auth = FirebaseAuth.getInstance()
+        if (auth.currentUser != null) {
+            signOut(this)
+        } else {
+            firebaseAuth()
+        }
+
+    }
+
+    private fun firebaseAuth() {
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),
+            RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                // Successfully signed in
+                val user = FirebaseAuth.getInstance().currentUser
+                val snack = Snackbar.make(findViewById<View>(R.id.rootLayout),"Logged In",Snackbar.LENGTH_LONG)
+                snack.show()
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
+        }
     }
 }
---
+++ app/src/main/java/ir/saatgroup/coursesscheduler/auth/firebase.kt
@@ -0,0 +1,20 @@
+package ir.saatgroup.coursesscheduler.auth
+
+import android.content.Context
+
+import com.firebase.ui.auth.AuthUI
+
+
+
+
+fun signOut(context: Context){
+    AuthUI.getInstance()
+        .signOut(context)
+        .addOnCompleteListener {
+
+        }
+}
+
+val providers = arrayListOf(
+    AuthUI.IdpConfig.GoogleBuilder().build(),
+    AuthUI.IdpConfig.AnonymousBuilder().build())
